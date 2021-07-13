bade53dddd205df8866cbda83dead274fd1b3b99
pratap1804 <pratap.1804@gmail.com>
2021-07-14 02:31:03 +0530

    Added: parameterised and default constructors and responding to the mood function accordingly

--- Exceptions/lib/src/main/java/Exceptions/MoodAnalyzer.java
+++ Exceptions/lib/src/main/java/Exceptions/MoodAnalyzer.java
@@ -1,6 +1,25 @@
 package Exceptions;
 
 public class MoodAnalyzer {
+	
+	String message;
+	
+	public MoodAnalyzer(String message) {
+		super();
+		this.message = message;
+	}
+	
+	public MoodAnalyzer() {
+		super();
+	}
+
+	public String analyseMood(String string) {
+		if(string  == "sad") {
+		return "SAD";
+		} else {
+			return "HAPPY";
+		}
+	}
     public static void main(String[]args) {
     	System.out.println("welcome to the exception handling!");
     }
--- Exceptions/lib/src/test/java/Exceptions/MoodAnalyzerTest.java
+++ Exceptions/lib/src/test/java/Exceptions/MoodAnalyzerTest.java
@@ -1,14 +1,19 @@
 package Exceptions;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MoodAnalyzerTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void HappyOrSadMessage() {
+	MoodAnalyzer object1 = new MoodAnalyzer("I am in Sad Mood");
+	String result1 = object1.analyseMood("sad");
+	Assertions.assertEquals("SAD",result1);
+	
+	MoodAnalyzer object2 = new MoodAnalyzer("I am in any Mood");
+	String result2 = object2.analyseMood("happy");
+	Assertions.assertEquals("HAPPY",result2);
+	
 	}
-
 }
