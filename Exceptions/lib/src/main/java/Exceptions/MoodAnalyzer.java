cc7a3886eccaf412c5686608d5214c8ad37ef494
pratap1804 <pratap.1804@gmail.com>
2021-07-14 02:03:35 +0530

    Added: Welcome message

---
+++ Exceptions/lib/src/main/java/Exceptions/MoodAnalyzer.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class MoodAnalyzer {
+    public static void main(String[]args) {
+    	System.out.println("welcome to the exception handling!");
+    }
+}
---
+++ Exceptions/lib/src/test/java/Exceptions/MoodAnalyzerTest.java
@@ -0,0 +1,14 @@
+package Exceptions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MoodAnalyzerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
